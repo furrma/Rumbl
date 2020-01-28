92eb2fec7d8b020b311fe0fb581b09aea5c9cc28
Ferma <olseend@gmail.com>
2020-01-28 17:35:39 +0800

    modified video_controller

--- web/models/video.ex
+++ web/models/video.ex
@@ -13,9 +13,18 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:" url", :title, :description])
-    |> validate_required([:" url", :title, :description])
+  # def changeset(struct, params \\ %{}) do
+  #   struct
+  #   |> cast(params, [:" url", :title, :description])
+  #   |> validate_required([:" url", :title, :description])
+  # end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w()
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
+  
 end
