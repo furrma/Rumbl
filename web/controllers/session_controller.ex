315e951524212a18a21742d688224b47f053c0f3
Ferma <olseend@gmail.com>
2020-01-28 15:56:35 +0800

    added delete session

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -18,4 +18,10 @@ defmodule Rumbl.SessionController do
         end
 
     end
+    # added delete session
+    def delete(conn, _) do
+        conn
+        |> Rumbl.Auth.logout()
+        |> redirect(to: page_path(conn, :index))
+    end
 end
